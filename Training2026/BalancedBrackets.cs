208022b1ac981de627b3beaa83b50ad28e0c6e25
Shanmugam <Ranjithkumar.Shanmugam@trumpf.com>
2026-07-16 10:47:46 +0530

    Write a function that returns true if a string contains correctly balanced brackets () and [],

---
+++ Training2026/BalancedBrackets.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+
+public class Program 
+   {
+   public static bool BalancedBrackets (string str) {
+      Stack<char> stack = new Stack<char> ();
+      Dictionary<char, char> pairs = new Dictionary<char, char>
+      {
+         { ')', '(' },
+            { ']', '[' }
+        };
+
+      foreach (char ch in str) {
+         if (ch == '(' || ch == '[') {
+            stack.Push (ch);
+         } else if (ch == ')' || ch == ']') {
+            if (stack.Count == 0 || stack.Pop () != pairs[ch]) {
+               return false;
+            }
+         }
+      }
+
+      return stack.Count == 0;
+   }
+
+   public static void Main () {
+      Console.WriteLine (BalancedBrackets ("([])"));
+      Console.WriteLine (BalancedBrackets ("(]"));
+   }
+}
+
